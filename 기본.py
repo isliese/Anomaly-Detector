d8e28241859a897fb202f375f4b28c5f4c03559c
KJW <duddo5000@naver.com>
2025-08-23 15:26:46 +0900

    Delete 기본.py

--- 기본.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-df = duckdb.sql(f"""
-                select *
-                from read_csv_auto('./Anomaly_Detection/candle_data/YFI_1chart.csv')
-                """).df()
-
-print(df)
